0f5ad09483cc3a9895dff17c16a3e1716e6e3771
Gabriel Rodrigues <gabriel.pr07@hotmail.com>
2021-04-24 22:14:40 -0300

    feat: adding Context Api's

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,11 @@ import '../styles/global.scss'
 import Header from '../components/Header'
 import styles from '../styles/app.module.scss'
 import Player from '../components/Player'
+import { PlayerContext } from '../context/PlayerContext'
 
 function MyApp({ Component, pageProps }) {
   return (
+    <PlayerContext.Provider value={'Diego'}>
     <div className={styles.wrapper}>
       <main>
         <Header/>
@@ -13,6 +15,7 @@ function MyApp({ Component, pageProps }) {
       </main>
       <Player/>
     </div>
+    </PlayerContext.Provider>
   )
 }
 
--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -51,8 +51,23 @@ export default function Episode({episode}: EpisodeProps){
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limits: 2,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+
+  const paths = data.map(episode => {
+    return {
+      params: episode.id
+    }
+  })
+
+
   return {
-    paths:[],
+    paths,
     fallback: 'blocking'
   }
 }
